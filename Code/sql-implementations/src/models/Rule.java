1f20828c9a987a8f1dfdb3e2b8a9f4ecdcad2e9e
Tim Gutberlet <t.gutberlet01@gmail.com>
2022-11-04 18:44:58 +1100

    Rule Comment

--- Code/sql-implementations/src/models/Rule.java
+++ Code/sql-implementations/src/models/Rule.java
@@ -3,33 +3,62 @@ package models;
 import java.util.List;
 
 /**
+ * Class that represents a rule
+ *
  * @author tgutberl
  */
 public class Rule {
   private Triple head;
   private List<Triple> body;
 
+  /**
+   * Costructor for setting
+   * @param head Triple representation of Head
+   * @param body Triple Array representation of Body
+   */
   public Rule(Triple head, List<Triple> body){
     this.head = head;
     this.body = body;
   }
 
+  /**
+   * Returns the body
+   *
+   * @return Body list representation
+   */
   public List<Triple> getBody() {
     return body;
   }
 
+  /**
+   * Returns the head
+   *
+   * @return Returns the Head triple representation
+   */
   public Triple getHead() {
     return head;
   }
 
+  /**
+   * Sets the body
+   * @param body Arraylist representation
+   */
   public void setBody(List<Triple> body) {
     this.body = body;
   }
 
+  /**
+   * Sets the Head representsation
+   * @param head Head triple representation
+   */
   public void setHead(Triple head) {
     this.head = head;
   }
 
+  /**
+   * Returns a String representation
+   * @return String
+   */
   @Override
   public String toString() {
     return "Rule{" +
